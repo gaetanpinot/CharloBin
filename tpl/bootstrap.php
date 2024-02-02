5f2364b5ad5c7da31d70c41fcbc39fe0c3bbdd4e
GaetanPinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-02-02 09:44:22 +0100

    rajout du footer pour pouvoir refaire le pr a cause d'une erreur de branch

--- tpl/bootstrap.php
+++ tpl/bootstrap.php
@@ -577,6 +577,9 @@ endif;
 					<span class="small"><?php echo I18n::_('In case this message never disappears please have a look at <a href="%s">this FAQ for information to troubleshoot</a>.', 'https://github.com/PrivateBin/PrivateBin/wiki/FAQ#why-does-the-loading-message-not-go-away'); ?></span>
 				</div>
 			</section>
+<footer>
+super footer
+</footer>
 					</main>
 <?php
 if ($DISCUSSION) :
